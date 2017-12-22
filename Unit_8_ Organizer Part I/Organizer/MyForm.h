4417c15a7e162092207b88e7d4cbffc9c0c2cbc6
gloombo <34732930+gloombo@users.noreply.github.com>
2017-12-21 19:07:58 -0500

    Add files via upload
    Part one

---
+++ Unit_8_ Organizer Part I/Organizer/MyForm.h
@@ -0,0 +1,147 @@
+#pragma once
+
+namespace Organizer {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+	using namespace System::Xml;
+
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+
+		DataSet^dsList;
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::MenuStrip^  menuStrip1;
+	protected:
+	private: System::Windows::Forms::ToolStripMenuItem^  fileToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  exitToolStripMenuItem;
+	private: System::Windows::Forms::DataGridView^  MyGrid;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Item_Name;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^  Category;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->fileToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->exitToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->MyGrid = (gcnew System::Windows::Forms::DataGridView());
+			this->Item_Name = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Category = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->menuStrip1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->MyGrid))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->ImageScalingSize = System::Drawing::Size(20, 20);
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->fileToolStripMenuItem });
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(482, 28);
+			this->menuStrip1->TabIndex = 0;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this->fileToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->exitToolStripMenuItem });
+			this->fileToolStripMenuItem->Name = L"fileToolStripMenuItem";
+			this->fileToolStripMenuItem->Size = System::Drawing::Size(44, 24);
+			this->fileToolStripMenuItem->Text = L"File";
+			// 
+			// exitToolStripMenuItem
+			// 
+			this->exitToolStripMenuItem->Name = L"exitToolStripMenuItem";
+			this->exitToolStripMenuItem->Size = System::Drawing::Size(108, 26);
+			this->exitToolStripMenuItem->Text = L"Exit";
+			this->exitToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::exitToolStripMenuItem_Click);
+			// 
+			// MyGrid
+			// 
+			this->MyGrid->AllowUserToAddRows = false;
+			this->MyGrid->AllowUserToDeleteRows = false;
+			this->MyGrid->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->MyGrid->Location = System::Drawing::Point(12, 31);
+			this->MyGrid->Name = L"MyGrid";
+			this->MyGrid->ReadOnly = true;
+			this->MyGrid->RowTemplate->Height = 24;
+			this->MyGrid->Size = System::Drawing::Size(458, 310);
+			this->MyGrid->TabIndex = 1;
+			// 
+			// Item_Name
+			// 
+			this->Item_Name->HeaderText = L"Item_Name";
+			this->Item_Name->Name = L"Item_Name";
+			// 
+			// Category
+			// 
+			this->Category->HeaderText = L"Category";
+			this->Category->Name = L"Category";
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(482, 353);
+			this->Controls->Add(this->MyGrid);
+			this->Controls->Add(this->menuStrip1);
+			this->MainMenuStrip = this->menuStrip1;
+			this->Name = L"MyForm";
+			this->Text = L"MyOrganizer";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->MyGrid))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void exitToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
+		Close();
+	}
+	private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) {
+		dsList = gcnew DataSet("list");
+		this->dsList->ReadXml("Data.xml");
+		this->MyGrid->DataSource = dsList;
+		this->MyGrid->DataMember = "item";
+	}
+};
+}
